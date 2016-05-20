770d4822694203bfdeb67f174f039f6e499d058c
demg <demg@DESKTOP-7HFQG9F>
2016-05-20 17:31:21 -0500

    Add project files.

---
+++ wsBibliotecaTest/Libros.aspx
@@ -0,0 +1,67 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Libros.aspx.cs" Inherits="wsBibliotecaTest.Libros" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+        <asp:Label ID="lblBienvenida" runat="server"></asp:Label>
+        <br />
+        <br />
+        <asp:GridView ID="GridView1" runat="server">
+        </asp:GridView>
+        <br />
+        <asp:RadioButtonList ID="RadioButtonList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="RadioButtonList1_SelectedIndexChanged">
+            <asp:ListItem Value="1">Añadir</asp:ListItem>
+            <asp:ListItem Value="2">Actualizar</asp:ListItem>
+            <asp:ListItem Value="3">Eliminar</asp:ListItem>
+        </asp:RadioButtonList>
+        <br />
+        <asp:Label ID="lblResultado" runat="server"></asp:Label>
+        <br />
+        <asp:Panel ID="pAñadir" runat="server" GroupingText="Añadir" Visible="False">
+            ISBN:<asp:TextBox ID="TextBox7" runat="server" Width="190px"></asp:TextBox>
+            &nbsp;Nombre:<asp:TextBox ID="TextBox8" runat="server" Width="190px"></asp:TextBox>
+            <br />
+            Autor:<asp:TextBox ID="TextBox9" runat="server" Width="190px"></asp:TextBox>
+            &nbsp;Editorial:<asp:TextBox ID="TextBox10" runat="server" Width="190px"></asp:TextBox>
+            <br />
+            Edicion:<asp:TextBox ID="TextBox11" runat="server" Width="100px"></asp:TextBox>
+            &nbsp;Año:<asp:TextBox ID="TextBox12" runat="server" Width="100px"></asp:TextBox>
+            <br />
+            <asp:Button ID="Button3" runat="server" OnClick="Button3_Click" Text="Añadir" />
+        </asp:Panel>
+        <br />
+        <asp:Panel ID="pActualizar" runat="server" GroupingText="Actualizar" Visible="False">
+            Seleccionar ISBN:<asp:DropDownList ID="DropDownList1" runat="server" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged" Width="200px">
+            </asp:DropDownList>
+            <br />
+            <br />
+            ISBN:<asp:TextBox ID="TextBox1" runat="server" ReadOnly="True" Width="190px"></asp:TextBox>
+            <br />
+            Nombre:<asp:TextBox ID="TextBox2" runat="server" Width="190px"></asp:TextBox>
+            &nbsp;Autor:<asp:TextBox ID="TextBox3" runat="server" Width="190px"></asp:TextBox>
+            <br />
+            Editorial:<asp:TextBox ID="TextBox4" runat="server" Width="190px"></asp:TextBox>
+            &nbsp;Edicion:<asp:TextBox ID="TextBox5" runat="server" Width="100px"></asp:TextBox>
+            <br />
+            Año:<asp:TextBox ID="TextBox6" runat="server" Width="100px"></asp:TextBox>
+            <br />
+            <asp:Button ID="Button1" runat="server" OnClick="Button1_Click" Text="Actualizar" />
+        </asp:Panel>
+        <br />
+        <asp:Panel ID="pEliminar" runat="server" GroupingText="Eliminar" Visible="False">
+            Seleccionar ISBN a eliminar:<asp:DropDownList ID="DropDownList2" runat="server" Width="200px">
+            </asp:DropDownList>
+            &nbsp;<asp:Button ID="Button2" runat="server" OnClick="Button2_Click" Text="Eliminar" />
+        </asp:Panel>
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ wsBibliotecaTest/Libros.aspx.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace wsBibliotecaTest
+{
+    public partial class Libros : System.Web.UI.Page
+    {
+
+        wsBiblioteca.webServiceDB wsBiblioteca = new wsBibliotecaTest.wsBiblioteca.webServiceDB();
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            lblBienvenida.Text = wsBiblioteca.bienvenida();
+            cargarLibros();
+        }
+
+        private void cargarLibros()
+        {
+            DataSet dt = new DataSet();
+            dt = wsBiblioteca.selectAll();
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+        }
+
+        protected void RadioButtonList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            switch (RadioButtonList1.SelectedIndex)
+            {
+                case 0:
+                    lblResultado.Text = "";
+                    pAñadir.Visible = true;
+                    pActualizar.Visible = false;
+                    pEliminar.Visible = false;
+                    break;
+                case 1:
+                    lblResultado.Text = "";
+                    pAñadir.Visible = false;
+                    pActualizar.Visible = true;
+                    pEliminar.Visible = false;
+
+                    DropDownList1.DataSource = wsBiblioteca.selectISBN();
+                    DropDownList1.DataValueField = "isbn";
+                    DropDownList1.DataTextField = "isbn";
+                    DropDownList1.DataBind();
+                    break;
+                case 2:
+                    lblResultado.Text = "";
+                    pAñadir.Visible = false;
+                    pActualizar.Visible = false;
+                    pEliminar.Visible = true;
+
+                    DropDownList2.DataSource = wsBiblioteca.selectISBN();
+                    DropDownList2.DataValueField = "isbn";
+                    DropDownList2.DataTextField = "isbn";
+                    DropDownList2.DataBind();
+                    break;
+            }
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            Object[] updateLibros = new Object[6];
+            updateLibros[0] = TextBox1.Text;
+            updateLibros[1] = TextBox2.Text;
+            updateLibros[2] = TextBox3.Text;
+            updateLibros[3] = TextBox4.Text;
+            updateLibros[4] = TextBox5.Text;
+            updateLibros[5] = TextBox6.Text;
+
+            lblResultado.Text = wsBiblioteca.update(updateLibros);
+            Response.Redirect(Request.RawUrl);
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataSet dt = new DataSet();
+            dt = wsBiblioteca.selectLibroByISBN(DropDownList1.SelectedValue);
+
+            TextBox1.Text = dt.Tables[0].Rows[0]["isbn"].ToString();
+            TextBox2.Text = dt.Tables[0].Rows[0]["nombreLibro"].ToString();
+            TextBox3.Text = dt.Tables[0].Rows[0]["autorLibro"].ToString();
+            TextBox4.Text = dt.Tables[0].Rows[0]["editorialLibro"].ToString();
+            TextBox5.Text = dt.Tables[0].Rows[0]["edicionLibro"].ToString();
+            TextBox6.Text = dt.Tables[0].Rows[0]["añoLibro"].ToString();
+        }
+
+        protected void Button2_Click(object sender, EventArgs e)
+        {
+            lblResultado.Text = wsBiblioteca.delete(DropDownList2.SelectedValue);
+            Response.Redirect(Request.RawUrl);
+        }
+
+        protected void Button3_Click(object sender, EventArgs e)
+        {
+            Object[] insertLibros = new Object[6];
+            insertLibros[0] = TextBox7.Text;
+            insertLibros[1] = TextBox8.Text;
+            insertLibros[2] = TextBox9.Text;
+            insertLibros[3] = TextBox10.Text;
+            insertLibros[4] = TextBox11.Text;
+            insertLibros[5] = TextBox12.Text;
+
+            if (!wsBiblioteca.insert(insertLibros))
+            {
+                lblResultado.Text = "Libro NO Añadido de forma correcta";
+            }
+            else
+            {
+                lblResultado.Text = "Libro Añadido de forma correcta";
+            }
+        }
+    }
+}
